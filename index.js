4c14d7ad19204ca317ce49d3d29362657e19e52d
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-31 09:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function setCart(c) {
 
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100 + 1)
-  cart.push({[item] : price})
+  var itemName = { [item] : price }
+  cart.push(itemName)
   console.log(`${item} has been added to your cart.`)
   return cart
 }
